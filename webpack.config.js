dfd4b732a6c1e91eb1e06e9d8ed4690476f723ae
AnatoliyPologovoy <pologovoy89@mail.ru>
2023-02-09 06:49:26 +0300

    tying fix bug to path for link by icon leaflet

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,21 @@ module.exports = {
   devtool: 'source-map',
   output: {
     filename: 'main.bundle.js',
-    path: path.resolve(__dirname, 'build/js'),
+    path: path.resolve(__dirname, 'docs/js'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
   },
 };
